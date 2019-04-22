94dba0d59a206c1be7eb47cac2cd199fc366edfb
JunC <junc0304@gmail.com>
2019-04-22 16:10:27 -0700

    start script added

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -8,6 +8,8 @@ module.exports = {
     },
     ACCOUNT_TYPE: ['local', 'google', 'facebook'],
     PORT : process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
-    HOST : process.env.OPENSHIFT_NODEJS_IP || 'localhost',
-    DATABASE_URL : process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME || 'mongodb://localhost/karma'
+    HOST : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    DATABASE_URL : 
+        'mongodb://'+ process.env.OPENSHIFT_MONGODB_DB_URL +
+        process.env.OPENSHIFT_APP_NAME || 'mongodb://127.0.0.1:27017/karma'
 }
