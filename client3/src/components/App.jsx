cdae93c58e0ee821499d5d8962a71ad3b4ae2340
JunC <junc0304@gmail.com>
2019-07-29 02:24:34 -0700

    apply fix to security issue reported by git

--- client3/src/components/App.jsx
+++ client3/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Jumbotron } from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
 
 import Header from './layout/Header.jsx';
 import Footer from './layout/Footer.jsx';
 import * as actions from '../actions';
 import BG from './images/Rocky4.jpg'
+
 const App = (props) => {
   return (
     <div className="App" style={{ minHeight: window.innerHeight, minWidth: "370px", backgroundImage:`url(${BG})` }}>
