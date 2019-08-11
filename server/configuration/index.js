bed6b944674e31d63ed995ecb9c4130097ad4058
JunC <junc@JunCs-MacBook-Pro.local>
2019-08-10 17:58:56 -0700

    temp for deploy

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -1,14 +1,14 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'dev',
 
-    SERVER_PORT : 4000,
-    DATABASE_URL :  'mongodb://localhost/karmaDB',
+    SERVER_PORT : process.env.SERVER_PORT || 3000,
+    DATABASE_URL : process.env.MONGO_URL || 'mongodb://localhost/karmaDB',
 
     JWT_SECRET: process.env.JWT_SECRET || 'testJWT',
     JWT_EXPIRY: process.env.JWT_EXPIRY || 1,
-    TOKEN_ISSUER:'JCJC',
+    TOKEN_ISSUER: process.env.TOKEN_ISSUER || 'JCJC',
 
-    COOKIE_TOKEN: 'karma-cookie',
+    COOKIE_TOKEN: process.env.COOKIE_TOKEN || 'cookie_token',
     BOARD_TYPE: ['NOTICE', 'MEETING', 'EVENT', 'DISCUSSION'],
     PAGE_TYPE: ['HOME', 'SUMMARY', 'HISTORY', 'MEMBERSHIP'],
     USER_ROLES: {
@@ -16,9 +16,10 @@ module.exports = {
       ADMIN:'ADMIN',
       USER: 'USER',
     },
+    
     COMMENT_STATUS: ['ACTIVE', 'INACTIVE', 'HIDDEN'],
 
-    ADMIN_ROLES: ['owner', 'admin'],
+    ADMIN_ROLES: ['OWNER', 'ADMIN'],
 
     ACCOUNT_TYPE: ['local', 'google', 'facebook']
    
